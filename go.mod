5176fb619297e26e4bc93ac3baa7edc80c103e92
Todd Campbell <todd@toddcampbell.net>
2020-04-19 17:58:48 -0700

    add a few tests
    Signed-off-by: Todd Campbell <todd@toddcampbell.net>

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/sensu-community/sensu-plugin-sdk v0.6.0
 	github.com/sensu/sensu-go v0.0.0-20200131164840-40b1d5938251
 	github.com/soniah/gosnmp v1.24.0
+	github.com/stretchr/testify v1.4.0
 )
--- main_test.go
+++ main_test.go
@@ -2,7 +2,49 @@ package main
 
 import (
 	"testing"
+
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
+/* Tests still needed:
+   getClientIP - need to build event.Check with network interfaces
+   executeCheck - borrow trap listener from gosnmp tests?
+*/
+
+func TestCheckArgs(t *testing.T) {
+	assert := assert.New(t)
+	event := corev2.FixtureEvent("entity1", "check1")
+	plugin.Version = "2"
+	plugin.Host = "localhost"
+	assert.NoError(checkArgs(event))
+	plugin.Version = "99"
+	assert.Error(checkArgs(event))
+}
+
+func TestContains(t *testing.T) {
+	assert := assert.New(t)
+	a := []string{"here", "there", "everywhere"}
+	assert.True(contains(a, "there"))
+	assert.False(contains(a, "nowhere"))
+}
+
+func TestFormatMessage(t *testing.T) {
+	assert := assert.New(t)
+	event := corev2.FixtureEvent("entity1", "check1")
+	event.Check.State = "passing"
+	event.Check.Output = "Check Output"
+	plugin.VarbindTrim = 100
+	expectedString := formatMessage(event)
+	assert.Equal(expectedString, "RESOLVED - entity1/check1 : Check Output")
+}
+
+func TestTrimOutput(t *testing.T) {
+	assert := assert.New(t)
+	plugin.VarbindTrim = 32
+	stringToTrim := "This is less than 32 characters"
+	assert.Equal(stringToTrim, trimOutput(stringToTrim))
+	plugin.VarbindTrim = 20
+	stringToTrim = "This is more than 32 characters"
+	assert.Equal("This is more than 32...", trimOutput(stringToTrim))
 }
